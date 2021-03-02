35930a48689b1104bba944f364cdf67a28daa877
Alexandre Beauchamp <abeauchamp@pulsewave.dev>
2021-03-01 23:13:54 -0500

    Added project

---
+++ Mutation/Mutation.Services.Tests/BarServiceTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mutation.Services.Tests
+{
+    [TestClass]
+    public class BarServiceTests
+    {
+        private readonly IBarService service;
+
+        public BarServiceTests() => service = new BarService();
+
+        [TestMethod]
+        public void IsPersonOldEnough_ShouldReturnTrue_WhenAgeIsGreaterThan18()
+        {
+            var isOldEnough = service.IsPersonOldEnough(20);
+
+            isOldEnough.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void IsPersonOldEnough_ShouldReturnFalse_WhenAgeIsLessThan18()
+        {
+            var isOldEnough = service.IsPersonOldEnough(12);
+
+            isOldEnough.Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void IsPersonOldEnough_ShouldReturnTrue_WhenAgeIsEqualTo18()
+        {
+            var isOldEnough = service.IsPersonOldEnough(18);
+
+            isOldEnough.Should().BeTrue();
+        }
+    }
+}
---
+++ Mutation/Mutation.Services/IBarService.cs
@@ -0,0 +1,7 @@
+﻿namespace Mutation.Services
+{
+    public interface IBarService
+    {
+        bool IsPersonOldEnough(int age);
+    }
+}
